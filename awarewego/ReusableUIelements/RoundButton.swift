be35619b49eff737dcceaaf344178c34f1ec3479
George Termentzoglou <geoter@outlook.com>
2019-09-12 14:17:50 +0300

    default value of round button

--- awarewego/ReusableUIelements/RoundButton.swift
+++ awarewego/ReusableUIelements/RoundButton.swift
@@ -11,7 +11,7 @@ import UIKit
 @IBDesignable
 class RoundButton: UIButton {
     
-    @IBInspectable var cornerRadius: CGFloat = 0{
+    @IBInspectable var cornerRadius: CGFloat = 19{
         didSet{
             self.layer.cornerRadius = cornerRadius
         }
