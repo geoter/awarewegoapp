b4f907f662d896bfc32e713c09765ed6e013d854
George Termentzoglou <geoter@outlook.com>
2019-10-03 13:45:53 +0300

    search recommendations ui

--- awarewego/Main Screens/Missions/Search/SearchRecommendationsVC.swift
+++ awarewego/Main Screens/Missions/Search/SearchRecommendationsVC.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class SearchRecommendationsVC: UIViewController {
 
+    @IBOutlet weak var collectionV: UICollectionView!
+    
+    var recommendedTags:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        recommendedTags.append(contentsOf: ["Nearby","Athens","Greece","Santorini","Parthenon"])
+        self.collectionV.reloadData()
     }
     
 
@@ -27,4 +32,28 @@ class SearchRecommendationsVC: UIViewController {
     }
     */
 
+}
+
+extension SearchRecommendationsVC: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("selected \(recommendedTags[indexPath.item])")
+    }
+}
+
+extension SearchRecommendationsVC: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return recommendedTags.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "TagCell", for: indexPath)
+        let label:UILabel = cell.viewWithTag(22) as! UILabel
+        label.text = recommendedTags[indexPath.item]
+        
+        let roundView:UIView = cell.viewWithTag(33)!
+        roundView.layer.cornerRadius = 16
+        return cell
+    }
+    
+    
 }
